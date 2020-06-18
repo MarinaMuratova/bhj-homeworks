ff265cdff9b170001a3585dc03637c18dceb2186
Marina Muratova <frau.muratova@gmail.com>
2020-06-18 22:52:08 +0300

    поменяла className на classList

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,7 +5,7 @@ const lostStart = document.getElementById("lost");
 let deadNumber = parseInt(deadStart.textContent);
 let lostNumber = parseInt(lostStart.textContent);
 for (let i = 0; i < holeList.length; i++){
-   let rightHole = holeList.className.includes("hole_has-mole");
+   let rightHole = holeList[i].classList.contains("hole_has-mole");
    holeList[i].onclick = function(){
    		if(rightHole){
 		  deadNumber++;
