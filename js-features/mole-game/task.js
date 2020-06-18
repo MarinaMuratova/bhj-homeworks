ba74817fd0a410b1159fdf2d10aa8aadb0a3f75a
Marina Muratova <frau.muratova@gmail.com>
2020-06-18 22:31:56 +0300

    правка в 1 дз, задание 3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,17 +4,16 @@ const lostStart = document.getElementById("lost");
 
 let deadNumber = parseInt(deadStart.textContent);
 let lostNumber = parseInt(lostStart.textContent);
-
-function deadOrLost(){
- let rightHole = holeList.className.includes("hole_has-mole");
-   for (let i = 0; i < holeList.length; i++){
-       if(rightHole){
-		  deadNumber+=1;
+for (let i = 0; i < holeList.length; i++){
+   let rightHole = holeList.className.includes("hole_has-mole");
+   holeList[i].onclick = function(){
+   		if(rightHole){
+		  deadNumber++;
 		  deadStart.textContent = deadNumber;
 	    } esle {
-           lostNumber+=1;
+           lostNumber++;
            lostStart.textContent = lostNumber;
         }
     }
 }
-hole.onclick = deadOrLost;
+
