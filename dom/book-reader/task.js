03e5eee15f968c2193779527674f5743c624fbbc
Marina Muratova <frau.muratova@gmail.com>
2020-06-25 16:51:19 +0300

    добавлен вариант в книгу

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,11 +2,13 @@ const fontSize = Array.from(document. querySelectorAll('a.font-size'));
 
 fontSize.forEach((elem) => {
 	elem.addEventListener('click', function(){
-		for(let i = 0; i < fontSize.length; i++){
-			if(fontSize[i].contains('font-size_active')){
-               fontSize[i].remove('font-size_active');
-		    }
-		}
+		// for(let i = 0; i < fontSize.length; i++){
+		// 	if(fontSize[i].contains('font-size_active')){
+  //              fontSize[i].remove('font-size_active');
+		//     }
+		// }
+		let activeSize = document.querySelector('.font-size_active');
+		activeSize.classList.remove('font-size_active');
 		this.classList.add('font-size_active');
 		if(this.contains("font-size_small")){
 			let smallSize = this.closest('.book');
