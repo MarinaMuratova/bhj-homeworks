81da2727553b201ed44ace683352d8b54f43f3ff
Marina Muratova <frau.muratova@gmail.com>
2020-06-25 08:51:05 +0300

    событие в меню изменено

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,7 +1,7 @@
 const menuItems = Array.from(document.querySelectorAll("li.menu__item"));
 
 menuItems.forEach((elem) => {
-	elem.addEventListener('mouseover', function(evt){
+	elem.addEventListener('click', function(evt){
 		let menu = this.querySelector('.menu_sub');
 	    if(menu){
 	  	    menu.classList.add('menu_active');
