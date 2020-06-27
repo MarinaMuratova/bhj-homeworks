5049aff72f797df9aa3f34ff12b3e67df4d693b0
Marina Muratova <frau.muratova@gmail.com>
2020-06-27 14:04:40 +0300

    переделала interests

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,11 +1,21 @@
 const dropdownItems = Array.from(document.querySelectorAll(".dropdown__item"));
-const dropdownValue = document.querySelector(".dropdown__value");
+const dropdown = Array.from(document.querySelectorAll(".dropdown"));
 
 
 dropdownItems.forEach((elem) =>  {
-	elem.addEventListener('click', function(){
-		this.closest(".dropdown__list").classList.remove('dropdown__list_active');
-		
+	elem.addEventListener('click', function(e){
+		this.closest(".dropdown").childNodes[1].textContent = elem.textContent;
 	}); 
 });
+
+dropdown.forEach((elem) => {
+    elem.addEventListener('click', function(e){
+    	e.preventDefault();
+    	if(this.childNodes[3].classList.contains('dropdown__list_active')){
+    		this.childNodes[3].classList.remove('dropdown__list_active');
+    	}else{
+    		this.childNodes[3].classList.add('dropdown__list_active');
+    	}
+})
+})
 
--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,23 +1,14 @@
 const interestCheck = Array.from(document.querySelectorAll('.interest__check')); //нашла инпуты
 
-// interestCheck.forEach((elem) => {
-// 	let interestClosest = elem.closest('.interest'); //нашла родителя инпута
-// 	if(interestClosest.querySelector('.interests')){ //если у родителя инпута есть вложенный список
-// 		elem.addEventListener('change', function(){ //добавляем на него событие
-//            if(elem.checked){ //если на элементе галочка
-//               interestClosest.querySelectorAll('.interest__check').checked = true;//то ставим галочку на все дочерние
-//            }
-// 		}); 
-// 	}
-// })
-
-
 interestCheck.forEach((elem) => {
 	elem.addEventListener('change', function(){ //добавляем на него событие
-	    let interestClosest = elem.closest('.interest'); //нашла родителя инпута
+	    let interestClosest = elem.closest('.interest');
+	    let interestChecked = Array.from(interestClosest.querySelectorAll('.interest__check')); //нашла родителя инпута
 	    if(interestClosest.querySelector('.interests')){ //если у родителя инпута есть вложенный список
-		if(elem.checked){ //если на элементе галочка
-              interestClosest.querySelectorAll('.interest__check').checked = true;//то ставим галочку на все дочерние
+		   if(elem.checked){ //если на элементе галочка
+              interestChecked.forEach((elem) => {elem.checked = true});//то ставим галочку на все дочерние
+           }else{
+           	  interestChecked.forEach((elem) => {elem.checked = false});
            }
 		}; 
 	})
