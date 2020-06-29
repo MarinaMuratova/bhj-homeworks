48cc6150f6856f96bacfc967ace255b208da7f10
Marina Muratova <frau.muratova@gmail.com>
2020-06-29 17:43:03 +0300

     дз dom

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,5 +1,5 @@
 const rotatorCase = Array.from(document.querySelectorAll('span.rotator__case'));
-let i = 0;
+
 setInterval(() => {
     rotatorCase[i].classList.toggle('rotator__case_active');
     i++
--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,20 +1,20 @@
 const fontSize = Array.from(document. querySelectorAll('a.font-size'));
 
 fontSize.forEach((elem) => {
-	elem.addEventListener('click', function(){
-		// for(let i = 0; i < fontSize.length; i++){
-		// 	if(fontSize[i].contains('font-size_active')){
-  //              fontSize[i].remove('font-size_active');
-		//     }
-		// }
+	elem.addEventListener('click', function(e){
+		e.preventDefault();
+
 		let activeSize = document.querySelector('.font-size_active');
-		activeSize.classList.remove('font-size_active');
+		if(activeSize){
+			activeSize.classList.remove('font-size_active');
+		}
 		elem.classList.add('font-size_active');
-		if(elem.contains("font-size_small")){
+
+		if(elem.querySelector(".font-size_small")){
 			let smallSize = this.closest('.book');
 			smallSize.classList.add("book_fs-small");
 		}
-		if(elem.contains("font-size_big")){
+		if(elem.querySelector(".font-size_big")){
 			let bigSize = this.closest('.book');
 			bigSize.classList.add("book_fs-big");
 		}
