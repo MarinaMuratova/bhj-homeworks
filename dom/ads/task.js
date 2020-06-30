db6fa14d93ce030ff5aea7e8d02ab7f481b2bcad
Marina Muratova <frau.muratova@gmail.com>
2020-06-30 15:42:16 +0300

    дз 3

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,25 @@
-const rotatorCase = Array.from(document.querySelectorAll('span.rotator__case'));
+const rotatorCase = document.querySelectorAll('span.rotator__case');
+
+// setInterval(() => {
+
+//     rotatorCase[i].classList.toggle('rotator__case_active');
+//     i++
+//     rotatorCase[i].classList.toggle('rotator__case_active');
+//     if(i >= rotatorCase.length){
+//     	i = 0;
+//     }
+// }, 1000)
 
 setInterval(() => {
-    rotatorCase[i].classList.toggle('rotator__case_active');
-    i++
-    rotatorCase[i].classList.toggle('rotator__case_active');
-    if(i >= rotatorCase.length){
-    	i = 0;
-    }
+	let activeRotator = document.querySelector('.rotator__case_active');
+	if(activeRotator){
+		activeRotator.classList.remove('rotator__case_active')
+	}
+	let nextElement = activeRotator.nextElementSibling;
+	if(nextElement){
+		nextElement.classList.add('rotator__case_active');
+	} else{
+		
+	}
+
 }, 1000)
-
-
