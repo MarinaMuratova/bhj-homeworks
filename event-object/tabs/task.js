f7fbf80d9fe6a1d221f3fd68c12989d3b279cbe1
Marina Muratova <frau.muratova@gmail.com>
2020-07-03 12:49:27 +0300

    добавила второй вариант таба

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,17 +1,35 @@
+// вариант 1
+// const tabList = Array.from(document.querySelectorAll('.tab'));
+// const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
+// const tabNavigation = document.querySelector('.tab__navigation');
+// let tabTextContent = [];
+// 	for (let i = 0; i < tabList.length; i++){
+// 		tabTextContent.push(tabList[i].textContent);
+// 	}
+
+// tabList.forEach((elem) => {
+// 	elem.addEventListener('click', function(e){
+// 		document.querySelector(".tab_active").classList.remove("tab_active");
+// 		this.classList.add('tab_active'); 
+// 		let indexOfTab = tabTextContent.indexOf(document.querySelector(".tab_active").textContent);
+// 		document.querySelector(".tab__content_active").classList.remove("tab__content_active");
+// 		tabsContent[indexOfTab].classList.add("tab__content_active");
+// 	})
+// })
+
+//вариант 2
 const tabList = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
-const tabNavigation = document.querySelector('.tab__navigation');
-let tabTextContent = [];
-	for (let i = 0; i < tabList.length; i++){
-		tabTextContent.push(tabList[i].textContent);
-	}
 
 tabList.forEach((elem) => {
 	elem.addEventListener('click', function(e){
-		document.querySelector(".tab_active").classList.remove("tab_active");
+		let current = document.querySelector(".tab_active");
+		current.classList.remove("tab_active");
 		this.classList.add('tab_active'); 
-		let indexOfTab = tabTextContent.indexOf(document.querySelector(".tab_active").textContent);
+		let indexOfTab = tabList.indexOf(document.querySelector(".tab_active"));
 		document.querySelector(".tab__content_active").classList.remove("tab__content_active");
 		tabsContent[indexOfTab].classList.add("tab__content_active");
 	})
 })
+
+
