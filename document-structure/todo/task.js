7972e711a09286316624a1ab6e9cd5c9c60c1c82
Marina Muratova <frau.muratova@gmail.com>
2020-07-04 11:08:13 +0300

    дз структура документа

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,50 +1,27 @@
 const taskInput = document.getElementById("task__input");
 const tasksList = document.querySelector(".tasks__list");
+const taskRemove = Array.from(document.querySelectorAll(".task__remove"));
 
-// taskInput.addEventListener("keydown", function(e){
-// 	e.preventDefault();
-// 	if(e.keyCode === 13){
-// 		if(this.value.length > 0){
-// 			tasksList.insertAdjacentHTML("afterBegin", 
-// 			`<div class="task">
-//              <div class="task__title">
-//              ${this.value}
-//              </div>
-//              <a href="#" class="task__remove">&times;</a>
-//              </div>`);
-// 		}
-// 		taskInput.value = " ";
-// 	}
-// })
-
-// taskInput.addEventListener("keydown", function(e){
-// 	if(e.keyCode === 13){
-// 		if(this.value.length > 0){
-// 			tasksList.innerHTML +=
-// 			`<div class="task">
-//              <div class="task__title">
-//              ${e.target.value}
-//              </div>
-//              <a href="#" class="task__remove">&times;</a>
-//              </div>`;
-// 		}
-// 		taskInput.value = " ";
-// 	}
-// })
-const form = document.querySelector(".tasks__control")
-form.addEventListener("submit", e =>{
-	e.preventDefault();
+taskInput.addEventListener("keydown", function(e){
+	
+	if(e.keyCode === 13){
 		if(this.value.length > 0){
-			tasksList.innerHTML +=
+			tasksList.insertAdjacentHTML("afterBegin", 
 			`<div class="task">
              <div class="task__title">
-             ${e.target.value}
+             ${this.value}
              </div>
              <a href="#" class="task__remove">&times;</a>
-             </div>`;
-		}
+             </div>`);
+		} 
+        e.preventDefault();
 		taskInput.value = " ";
-	
+	}
 })
 
-
+taskRemove.forEach((elem) => {
+	elem.onclick = function(e){
+		e.preventDefault();
+		this.closest('task').remove();
+	}
+})
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,16 +3,15 @@ const hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"));
 hasTooltip.forEach((elem) => {
 	elem.addEventListener('click', function(e){
 		e.preventDefault();
+
 		let current = document.querySelector(".tooltip_active");
         if(current){
         	current.classList.remove("tooltip_active");
         } 
-        this.classList.add("tooltip_active");
-		// this.insertAdjacentHTML("afterEnd",`<div class="tooltip tooltip_active">${elem.title} </div>`);
-		let tooltip = document.querySelector(".tooltip_active");
+        this.nextElementSibling.classList.add("tooltip_active");
         let position = this.getBoundingClientRect();
-		tooltip.style.top = position.top;
-		tooltip.style.left = position.left;
 
+		this.nextElementSibling.style.top = `${position.top + 22}px`;
+		this.nextElementSibling.style.left = `${position.left}px`;
 	});
 })
